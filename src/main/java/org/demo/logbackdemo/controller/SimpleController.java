709667fff2a8ca77aff6257df60f68028e7f92d1
백재우 <9712jw@gmail.com>
2025-06-01 13:56:04 +0900

    :sparkles: 가장 간단한 로그 출력 예제

---
+++ src/main/java/org/demo/logbackdemo/controller/SimpleController.java
@@ -0,0 +1,22 @@
+package org.demo.logbackdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/simple")
+public class SimpleController {
+
+    @GetMapping
+    public String getSimpleMessage() {
+        log.trace("trace message");
+        log.debug("debug message");
+        log.info("info message");
+        log.warn("warn message");
+        log.error("error message");
+        return "Hello";
+    }
+}
