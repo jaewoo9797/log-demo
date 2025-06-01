fc9e784bc320d1a3ec80c6c78a1150f0a80293df
백재우 <9712jw@gmail.com>
2025-06-01 12:38:00 +0900

    project init

---
+++ src/test/java/org/demo/logbackdemo/LogbackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.logbackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
